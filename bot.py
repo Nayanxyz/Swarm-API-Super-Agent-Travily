c65b05da5b84276b2d9113cce208a8f23aece4f3
NayanPython <choudharyji527@gmail.com>
2026-05-09 12:06:37 +0530

    bot.py-02

--- bot.py
+++ bot.py
@@ -7,3 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+# [IMPORTANT]: Paste your actual Render URL here!
+API_URL = "https://swarm-api-super-agent-travily.onrender.com/chat"
+

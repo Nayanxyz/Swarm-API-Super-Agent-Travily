8ccdd7af0f6c414004c0c037fac909592c3efd4e
NayanPython <choudharyji527@gmail.com>
2026-05-09 12:06:51 +0530

    bot.py-03

--- bot.py
+++ bot.py
@@ -10,3 +10,9 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 # [IMPORTANT]: Paste your actual Render URL here!
 API_URL = "https://swarm-api-super-agent-travily.onrender.com/chat"
 
+# Give the bot permission to read messages
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
